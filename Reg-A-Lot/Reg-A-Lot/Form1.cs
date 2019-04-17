f79ed5dd81354d7640620cc7ba2d79a0283ef330
journeyman846 <jamescrafton@live.com>
2019-04-16 19:09:24 -0500

    New Project

---
+++ Reg-A-Lot/Reg-A-Lot/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Reg_A_Lot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
