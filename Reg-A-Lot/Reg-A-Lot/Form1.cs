7de1e51aec7b9168b524136446a46d0cd45ffcc5
programmer <programmer@BB21008>
2019-04-16 19:37:52 -0500

    Added a Person and Student class and put a comment block for referral inside the Windows Form

--- Reg-A-Lot/Reg-A-Lot/Form1.cs
+++ Reg-A-Lot/Reg-A-Lot/Form1.cs
@@ -11,8 +11,10 @@ namespace Reg_A_Lot
 {
     public partial class Form1 : Form
     {
+        // Person somePerson;
         public Form1()
         {
+            // somePerson = new Person();
             InitializeComponent();
         }
     }
