adf6141ac3b89625d5b0b6cde595f95b96111a33
Karalee Tiller <k0581300@amarillocollege.com>
2019-04-16 21:54:28 -0500

    Added specified characteristics for the Person class and some commented blocks of code to the Student class

--- Reg-A-Lot/Reg-A-Lot/Person.cs
+++ Reg-A-Lot/Reg-A-Lot/Person.cs
@@ -8,9 +8,14 @@ namespace Reg_A_Lot
     class Person
     {
         // Creating the characteristics of 'Person'
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public int Age { get; set; }
         public int CollegeID { get; set; }
+        public string Address { get; set; }
+        public int PhoneNumber { get; set; }
+        public int FaxNumber { get; set; }
+        public string Email { get; set; }
 
     }
 }
--- Reg-A-Lot/Reg-A-Lot/Student.cs
+++ Reg-A-Lot/Reg-A-Lot/Student.cs
@@ -5,7 +5,30 @@ using System.Text;
 
 namespace Reg_A_Lot
 {
-    class Student
+    class Student : Person
     {
+        // Declare any variables needed for the Student class
+        // Students need to be able to enter their first and last name, age, gender, collegeID, and address
+        // Students need to be able to register for classes
+        // Students will need to have access to quickly determine course availability and assigned professors
+        // Student schedule information is protected from unauthorized access.
+
+
+        /// <summary>
+        /// Up to 2000 students will use the Reg-A-Lot System each school term to register for courses and to 
+        /// review their final grades. The students are typically educated, computer literate, and have access
+        /// to the Internet. It is expected that each term 10% of the students will be registering at Made-up 
+        /// College for the first time and will be unfamiliar with the course registration process.
+        /// </summary>
+
+
+
+
+        public Student()
+        {
+            
+        }
+
+
     }
 }
