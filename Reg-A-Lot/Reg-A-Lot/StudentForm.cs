2cf9fbae44024c05861e32552f363f0f2bdc8621
Karalee Tiller <k0581300@amarillocollege.com>
2019-04-27 14:56:09 -0500

    Changed around the database implementation in StudentForm

--- Reg-A-Lot/Reg-A-Lot/StudentForm.cs
+++ Reg-A-Lot/Reg-A-Lot/StudentForm.cs
@@ -97,13 +97,19 @@ namespace Reg_A_Lot
         }
 
 
-        // Instantiating a SQL object
+        // Instantiating a SQL object and instantiating a Student object
         SqlConnection connection = new SqlConnection();
-        // Instantiating a Student object
         Student studentRegistration = new Student();
+        Database database = new Database();
+        DataTable userTable = new DataTable();
+        DataTable courseTable = new DataTable();
+        DataTable studentTable = new DataTable();
+        DataTable professorsTable = new DataTable();
+        SqlCommand cmd = new SqlCommand();
 
         private void btnStudentRegister_Click(object sender, EventArgs e)
         {
+            
             // Saving the student's registration information to the Student class variables
             studentRegistration.FirstName = txtStudentFirstName.Text;
             studentRegistration.LastName = txtStudentLastName.Text;
@@ -119,28 +125,41 @@ namespace Reg_A_Lot
             connection.Open();
 
 
-            using (SqlCommand insertNewStudent = connection.CreateCommand())
-            {
-               
-                // Inserting the student information to the student table in the database
-                insertNewStudent.CommandText = "insert into dbo.Students (\"FirstName\", \"LastName\", \"Age\", \"Email\", \"Address\", \"Phone\") values (@FirstName, @LastName, @Age, @Email, @Address, @Phone);";
-                insertNewStudent.Parameters.Add(new SqlParameter("FirstName", studentRegistration.FirstName));
-                insertNewStudent.Parameters.Add(new SqlParameter("LastName", studentRegistration.LastName));
-                insertNewStudent.Parameters.Add(new SqlParameter("Age", studentRegistration.Age));
-                insertNewStudent.Parameters.Add(new SqlParameter("Email", studentRegistration.Email));
-                insertNewStudent.Parameters.Add(new SqlParameter("Address", studentRegistration.Address));
-                insertNewStudent.Parameters.Add(new SqlParameter("Phone", studentRegistration.PhoneNumber));
-                insertNewStudent.ExecuteNonQuery();
-                
-            }
+            //using (SqlCommand insertNewStudent = connection.CreateCommand())
+            //{
 
-            using (SqlCommand insertNewStudentUserInfo = connection.CreateCommand())
-            {
-                // Inserting the username and password of the associated student to the Users table in the database
-                insertNewStudentUserInfo.CommandText = "insert into dbo.Users (\"UserName\", \"Password\") values (@UserName, @Password);";
-                insertNewStudentUserInfo.Parameters.Add(new SqlParameter("UserName", studentRegistration.UserName));
-                insertNewStudentUserInfo.Parameters.Add(new SqlParameter("Password", studentRegistration.Password));
-            }
+            //    // Inserting the student information to the student table in the database
+            //    insertNewStudent.CommandText = "insert into dbo.Students (\"FirstName\", \"LastName\", \"Age\", \"Email\", \"Address\", \"Phone\") values (@FirstName, @LastName, @Age, @Email, @Address, @Phone);";
+            //    insertNewStudent.Parameters.Add(new SqlParameter("FirstName", studentRegistration.FirstName));
+            //    insertNewStudent.Parameters.Add(new SqlParameter("LastName", studentRegistration.LastName));
+            //    insertNewStudent.Parameters.Add(new SqlParameter("Age", studentRegistration.Age));
+            //    insertNewStudent.Parameters.Add(new SqlParameter("Email", studentRegistration.Email));
+            //    insertNewStudent.Parameters.Add(new SqlParameter("Address", studentRegistration.Address));
+            //    insertNewStudent.Parameters.Add(new SqlParameter("Phone", studentRegistration.PhoneNumber));
+            //    insertNewStudent.ExecuteNonQuery();
+
+            //}
+
+            //using (SqlCommand insertNewStudentUserInfo = connection.CreateCommand())
+            //{
+
+            database.InsertStudent(studentRegistration.FirstName, studentRegistration.LastName, studentRegistration.Age, studentRegistration.Email, studentRegistration.Address, studentRegistration.PhoneNumber);
+            txtStudentFirstName.Clear();
+            txtStudentLastName.Clear();
+            txtStudentAge.Clear();
+            txtStudentEmail.Clear();
+            txtStudentAddress.Clear();
+            txtStudentPhoneNumber.Clear();
+
+            database.InsertUser(studentRegistration.UserName, studentRegistration.Password, "Student");
+            txtStudentUserName.Clear();
+            txtStudentPassword.Clear();
+                
+            //    // Inserting the username and password of the associated student to the Users table in the database
+            //    insertNewStudentUserInfo.CommandText = "insert into dbo.Users (\"UserName\", \"Password\") values (@UserName, @Password);";
+            //    insertNewStudentUserInfo.Parameters.Add(new SqlParameter("UserName", studentRegistration.UserName));
+            //    insertNewStudentUserInfo.Parameters.Add(new SqlParameter("Password", studentRegistration.Password));
+            //}
 
             // Displaying message box of successful student registration and opening Login Form
             MessageBox.Show("Registration was successful!");
